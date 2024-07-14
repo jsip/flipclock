5bf395b719ffbd0f73ead61f072d50ad252c1edd
jsip <jsimonroyer@gmail.com>
2024-07-14 18:31:06 -0400

    fix: pwa paths on gh pages

--- service-worker.js
+++ service-worker.js
@@ -2,19 +2,18 @@ self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open('flip-clock-cache-v1').then((cache) => {
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/main.js',
-        '/jscolor.js',
-        '/static/android-chrome-192x192.png',
-        '/static/android-chrome-512x512.png',
-        '/static/apple-touch-icon.png',
-        '/static/favicon-16x16.png',
-        '/static/favicon-32x32.png',
-        '/static/favicon.ico',
-        '/static/icon.png',
-        '/site.webmanifest'
+        'index.html',
+        'style.css',
+        'main.js',
+        'jscolor.js',
+        'static/android-chrome-192x192.png',
+        'static/android-chrome-512x512.png',
+        'static/apple-touch-icon.png',
+        'static/favicon-16x16.png',
+        'static/favicon-32x32.png',
+        'static/favicon.ico',
+        'static/icon.png',
+        'site.webmanifest'
       ]);
     })
   );
